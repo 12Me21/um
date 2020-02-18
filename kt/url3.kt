ef3f73a538416ee6e973cdfb8f12ccb1e46ef8fb
12Me21 <12me21.mc@gmail.com>
2020-02-18 13:19:30 -0500

    extra

--- kt/url3.kt
+++ kt/url3.kt
@@ -1,12 +1,10 @@
 @file:DependsOn("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
 @file:DependsOn("com.squareup.okhttp3:okhttp:4.3.1")
-@file:DependsOn("ru.gildor.coroutines:kotlin-coroutines-okhttp:1.0")
 
 import kotlinx.coroutines.*
 import okhttp3.*
 import java.util.concurrent.TimeUnit
 import java.io.IOException
-import ru.gildor.coroutines.okhttp.*
 
 val okHttpClient = OkHttpClient.Builder()
 	.readTimeout(0, TimeUnit.DAYS)
